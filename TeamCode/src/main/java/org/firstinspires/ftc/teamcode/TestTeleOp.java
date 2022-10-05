bfdc4988f94a3a7d4c30177a39f8f807bea6cc0f
Ketan Raghu <ketanaliraghu@gmail.com>
2022-10-05 01:14:18 -0400

    Added kinematics functions and acceleration to test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name = "Test SyndiOp")
 //@Disabled
@@ -17,6 +18,7 @@ public class TestTeleOp extends LinearOpMode {
     DcMotor BL;
     DcMotor BR;
 
+    @RequiresApi(api = Build.VERSION_CODES.N) // Needed for Functional programming
     @Override
     public void runOpMode() throws InterruptedException {
         int invertedControl = 1;
@@ -52,14 +54,20 @@ public class TestTeleOp extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
+            double time_counter = 0;
+
             while(gamepad1.right_stick_x != 0 ||
                     gamepad1.right_stick_y != 0 ||
                     gamepad1.left_stick_x != 0 ||
                     gamepad1.left_stick_y != 0) {
-                FR.setPower(y - x - rx);
-                FL.setPower(y + x + rx);
-                BR.setPower(y + x - rx);
-                BL.setPower(y - x + rx);
+
+                sleep(100);
+                time_counter += 100;
+
+                FR.setPower((y - x - rx) * ConstantFunction.POLY2_COLLAPSE.apply(time_counter));
+                FL.setPower((y + x + rx) * ConstantFunction.POLY2_COLLAPSE.apply(time_counter));
+                BR.setPower((y + x - rx) * ConstantFunction.POLY2_COLLAPSE.apply(time_counter));
+                BL.setPower((y - x + rx) * ConstantFunction.POLY2_COLLAPSE.apply(time_counter));
             }
 
             telemetry.update();
